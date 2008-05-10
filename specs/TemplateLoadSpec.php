e35917f70908e5c9403d07f9afdba89300496d80
kumatch <kumatch@21cb40e7-8b4c-0410-b6e2-a195dba76f6b>
2008-05-10 12:54:32 +0000

    - Added template loding specs.

---
+++ specs/TemplateLoadSpec.php
@@ -0,0 +1,102 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4: */
+
+/* 
+ * @package    Runemaster
+ * @copyright  2008 KUMAKURA Yousuke All rights reserved.
+ * @version    SVN: $Id$
+ * @since      File available since Release 0.1.0
+ */
+
+require_once dirname(__FILE__) . '/prepare.php';
+
+// {{{ Describeテンプレート読み込み
+
+/**
+ * テンプレート読み込みに関するSpec
+ *
+ * @package    Runemaster
+ * @copyright  2008 KUMAKURA Yousuke All rights reserved.
+ * @version    Release: @package_version@
+ * @since      Class available since Release 0.1.0
+ */
+class Describeテンプレート読み込み extends SpecCommon
+{
+
+    // {{{ properties
+
+    /**#@+
+     * @access public
+     */
+
+    /**#@-*/
+
+    /**#@+
+     * @access protected
+     */
+
+    /**#@-*/
+
+    /**#@+
+     * @access private
+     */
+
+    /**#@-*/
+
+    /**#@+
+     * @access public
+     */
+
+    public function itテンプレートはinlucdeで読み込んでいるのでphpタグが使える()
+    {
+        $master = $this->_master;
+        $master->assign(array('title' => 'Hello, World!'));
+        $display = rendererInTest($master, 'TemplateLoad/UsingPHP');
+        $result = file_get_contents('./results/TemplateLoad/UsingPHP.html');
+
+        $this->spec($display)->should->be($result);
+    }
+
+    public function itテンプレート内のphpスクリプトで変数が扱える()
+    {
+        $master = $this->_master;
+        $variables = array('items' => array('foo', 'bar', 'baz'));
+
+        $display = rendererInTest($master, 'TemplateLoad/UsingPHPWithVariable',
+                                  $variables
+                                  );
+        $result = file_get_contents('./results/TemplateLoad/UsingPHPWithVariable.html');
+
+        $this->spec($display)->should->be($result);
+    }
+
+    /**#@-*/
+
+    /**#@+
+     * @access protected
+     */
+
+    /**#@-*/
+
+    /**#@+
+     * @access private
+     */
+
+    /**#@-*/
+
+    // }}}
+}
+
+// }}}
+
+/*
+ * Local Variables:
+ * mode: php
+ * coding: utf-8
+ * tab-width: 4
+ * c-basic-offset: 4
+ * c-hanging-comment-ender-p: nil
+ * indent-tabs-mode: nil
+ * End:
+ */
+?>
